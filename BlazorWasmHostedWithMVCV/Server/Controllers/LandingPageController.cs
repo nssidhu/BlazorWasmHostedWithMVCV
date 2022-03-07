0c7fb71d086b3e1c001fe463b14f844a98b236b8
Naunihal Singh Sidhu <nssidhu@outlook.com>
2022-03-06 21:49:16 -0600

    Add project files.

---
+++ BlazorWasmHostedWithMVCV/Server/Controllers/LandingPageController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasmHostedWithMVCV.Server.Controllers
+{
+    public class LandingPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "This message is from MVC Controller";
+            // return Content("This message is from MVC Controller from updated");
+            return View();
+            //  return View("ViewName", model)
+            //return RedirectToAction("Index");
+            //return PartialView("PartialViewName", Model);
+            //return View("~/Views/FolderName/ViewName.aspx");
+        }
+    }
+}
---
+++ BlazorWasmHostedWithMVCV/Server/Controllers/QrCodeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace BlazorWasmHostedWithMVCV.Server.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        public IActionResult Index(string tokenGuid)
+        {
+            //https://github.com/codebude/QRCoder/
+
+            ViewBag.Message = "This message is from MVC QRCode Controller" + tokenGuid;
+            ViewBag.QrCode = tokenGuid;
+
+            QRCodeGenerator _qrCode = new QRCodeGenerator();
+            QRCodeData _qrCodeData = _qrCode.CreateQrCode(tokenGuid, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(_qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return View(BitmapToBytesCode(qrCodeImage));
+
+            // return Content("This message is from MVC Controller from updated");
+            //return View();
+            //  return View("ViewName", model)
+            //return RedirectToAction("Index");
+            //return PartialView("PartialViewName", Model);
+            //return View("~/Views/FolderName/ViewName.aspx");
+        }
+
+        [NonAction]
+        private static byte[] BitmapToBytesCode(Bitmap image)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                image.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+
+
+    }
+}
